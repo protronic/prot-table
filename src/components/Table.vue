a916cc5800add581a9aa88b14e636447153f9715
Robert Seidler <r.seidler@protronic-gmbh.de>
2019-05-10 11:05:37 +0200

    fixed equal on blacklisted functions

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -314,7 +314,8 @@ export default {
       }
       const result = {
         keys: header_fields.filter(value => {
-          return !this.table_options.dontShowCols.includes(value) && typeof value !== 'function'//|| !this.dont_schow.includes(value);
+          this.debug.push(value);
+          return !this.table_options.dontShowCols.includes(value) && typeof value === 'function'//|| !this.dont_schow.includes(value);
         }),
         display: display_names,
         widths: fixed_widths
